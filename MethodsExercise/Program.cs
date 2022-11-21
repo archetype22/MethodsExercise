c3e740178433a77124d67026baa89066534c51aa
nathan stephens <nstephens1234567@gmail.com>
2022-11-21 14:58:10 -0600

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();   
+            Console.WriteLine("What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your lucky number?");
+            int luckyNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{name}, you will see {luckyNumber} {favColor} {favAnimal}s this day, and your life will be complete!");
+
         }
     }
 }
