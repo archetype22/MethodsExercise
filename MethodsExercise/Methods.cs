cdd6bd843121dddd3d22f81593900239785a2501
nathan stephens <nstephens1234567@gmail.com>
2022-11-22 14:57:31 -0600

    added bonus

--- MethodsExercise/Methods.cs
+++ MethodsExercise/Methods.cs
@@ -24,4 +24,36 @@ internal static class Methods
     {
         return num1 - num2; 
     }
+
+    public static void AddWithParam(params int[] numbers)
+    {
+        int sum = 0;    
+        foreach (var item in numbers)
+        {
+            sum += item;    
+        }
+        Console.WriteLine(sum);
+    }
+
+    public static void MultiplyWithParam(params int[] numbers)
+    {
+        int answer = 1;
+        foreach (var item in numbers)
+        {
+            answer *= item;
+        }
+        Console.WriteLine(answer); 
+    }
+
+    public static void SubtractWithParam(params int[] numbers)
+    {
+        var startingPoint = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        { 
+            startingPoint -= numbers[i]; 
+        }
+        Console.WriteLine(startingPoint);
+    }
 }
+
+                                                  
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,21 +4,31 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("What is your name?");
-            //string name = Console.ReadLine();   
-            //Console.WriteLine("What is your favorite color?");
-            //string favColor = Console.ReadLine();
-            //Console.WriteLine("What is your favorite animal?");
-            //string favAnimal = Console.ReadLine();
-            //Console.WriteLine("What is your lucky number?");
-            //int luckyNumber = int.Parse(Console.ReadLine());
-            //Console.WriteLine($"{name}, you will see {luckyNumber} {favColor} {favAnimal}s this day, and your life will be complete!");
+            //Exercise 1 ~
+            #region
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();   
+            Console.WriteLine("What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your lucky number?");
+            int luckyNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{name}, you will see {luckyNumber} {favColor} {favAnimal}s this day, and your life will be complete!");
+            #endregion
 
-
+            //Exercise 2 ~
+            #region
             Console.WriteLine(Methods.Add(9, 3));
             Console.WriteLine(Methods.Multiply(9, 3));
             Console.WriteLine(Methods.Divide(9, 3)); 
             Console.WriteLine(Methods.Subtract(9, 3));
+            #endregion
+
+            //Bonus ~
+            Methods.AddWithParam(12, 11, 10, 9, 8, 7);
+            Methods.MultiplyWithParam(3, 3, 3, 3, 3);
+            Methods.SubtractWithParam(24, 12, 10, 2);
         }
     }
 }
